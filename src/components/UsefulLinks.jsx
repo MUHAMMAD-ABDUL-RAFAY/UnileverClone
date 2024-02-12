ad3662e7e0a1184003d4856b86f2d9f6f1adb01c
MUHAMMAD-ABDUL-RAFAY <k200180@nu.edu.pk>
2024-02-13 02:26:10 +0500

    added responsivness in usefulLinks component

--- src/components/UsefulLinks.jsx
+++ src/components/UsefulLinks.jsx
@@ -1,13 +1,18 @@
 import { Typography } from "@mui/material";
-import React from "react";
+import React,{useEffect} from "react";
 
 const UsefulLinks = () => {
+  const isSmallDevice = window.innerWidth < 480;
+  const isMediumDevice = window.innerWidth >= 480 && window.innerWidth < 768;
+  useEffect(() => {
+    console.log(isSmallDevice, isMediumDevice);
+  }, [isSmallDevice, isMediumDevice]);
   return (
     <div
-      className="mt-16 flex pb-16"
+      className={`mt-16 flex ${(isSmallDevice || isMediumDevice) ? 'flex-col' : "flex-row" } pb-16`}
       style={{ marginLeft: "10%", marginRight: "10%" }}>
-      <div style={{ width: "50%" }}>
-        <div className="flex flex-col gap-y-8">
+      <div style={{ width: (isSmallDevice || isMediumDevice) ? "85%" : "50%" }}>
+        <div className={`flex flex-col gap-y-8 ${(isSmallDevice || isMediumDevice) ? "mb-12" : ""} `}>
           <Typography style={{color:"#B5AE88",fontSize:"26px"}} >Nestle</Typography>
           <Typography style={{color:"#E5E0D8",fontSize:"24px"}}>
             We unlock the power of food to enhance quality of life for everyone,
@@ -16,7 +21,7 @@ const UsefulLinks = () => {
           <img className="mt-12" src="/nestlelogo.svg" width="120px" height="85px" style={{color:"#CFC18E"}} alt="Nestle Logo" />          
         </div>
       </div>
-      <div style={{ width: "50%" }}>
+      <div style={{ width: (isSmallDevice || isMediumDevice) ? "85%" : "50%" }}>
         <div className="flex flex-col gap-y-4" style={{}}>
             <hr style={{color:"#7D7B78"}} />
             <Typography style={{color:"#B5AE88",fontSize:"16px",fontWeight:"400"}} >Useful Links</Typography>
