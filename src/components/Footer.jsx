c71f49c62b2a882c603529abb5eef13beb06bcf2
MUHAMMAD-ABDUL-RAFAY <k200180@nu.edu.pk>
2024-02-13 03:04:26 +0500

    added responsiveness to the last component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,74 @@
-import React from "react";
-import CopyrightIcon from '@mui/icons-material/Copyright';
+import React, { useEffect } from "react";
+import CopyrightIcon from "@mui/icons-material/Copyright";
 const Footer = () => {
+  const isSmallDevice = window.innerWidth < 480;
+  const isMediumDevice = window.innerWidth >= 480 && window.innerWidth < 768;
+  useEffect(() => {
+    console.log(isSmallDevice, isMediumDevice);
+  }, [isSmallDevice, isMediumDevice]);
   return (
-    <div style={{ backgroundColor: "#2A2C2C",padding:"1% 10%" }}>
-      <div className="flex justify-evenly" style={{color:"#CFC8BD"}}>
-        <a> <CopyrightIcon /> 2023 Nestle</a>
-        |
-        <a>Site map</a>
-        |
-        <a>Help</a>
-        |
-        <a>Privacy</a>
-        |
-        <a>Cookies</a>
-        |
-        <a>Terms of use</a>
-        |
-        <a>Accessibility</a>
+    <div
+      className={`${
+        isMediumDevice || isSmallDevice ? "flex justify-between gap-x-16" : ""
+      }`}
+      style={{ backgroundColor: "#2A2C2C", padding: "1% 10%" }}
+    >
+      <div
+        className={`flex ${
+          isMediumDevice || isSmallDevice ? "flex-col w-[50%] text-center" : ""
+        } justify-evenly`}
+        style={{ color: "#CFC8BD" }}
+      >
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>
+          {" "}
+          <CopyrightIcon sx={{ fontSize: "15px" }} /> 2023 Nestle
+        </a>
+        {isSmallDevice || isMediumDevice ? <hr /> : "|"}
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>Site map</a>
+        {isSmallDevice || isMediumDevice ? <hr /> : "|"}
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>Help</a>
+        {isSmallDevice || isMediumDevice ? <hr /> : "|"}
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>Privacy</a>
+        {isSmallDevice || isMediumDevice ? <hr /> : "|"}
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>Cookies</a>
+        {isSmallDevice || isMediumDevice ? <hr /> : "|"}
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>
+          Terms of use
+        </a>
+        {isSmallDevice || isMediumDevice ? <hr /> : "|"}
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>
+          Accessibility
+        </a>
       </div>
-      <div className="flex justify-evenly" style={{color:"#CFC8BD"}}>
-        <a>Carbon efficiency</a>
-        |
-        <a>Suppliers</a>
-        |
-        <a>Social media</a>
-        |
-        <a>RSS</a>
-        |
-        <a>Sign up</a>
-        |
-        <a>Unsubscribe</a>
-        |
-        <a>Contact us</a>
+      <div
+        className={`flex ${
+          isMediumDevice || isSmallDevice ? "flex-col w-[50%] text-center" : ""
+        } justify-evenly`}
+        style={{ color: "#CFC8BD" }}
+      >
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>
+          Carbon efficiency
+        </a>
+        {isSmallDevice || isMediumDevice ? <hr /> : "|"}
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>
+          Suppliers
+        </a>
+        {isSmallDevice || isMediumDevice ? <hr /> : "|"}
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>
+          Social media
+        </a>
+        {isSmallDevice || isMediumDevice ? <hr /> : "|"}
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>RSS</a>
+        {isSmallDevice || isMediumDevice ? <hr /> : "|"}
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>Sign up</a>
+        {isSmallDevice || isMediumDevice ? <hr /> : "|"}
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>
+          Unsubscribe
+        </a>
+        {isSmallDevice || isMediumDevice ? <hr /> : "|"}
+        <a style={{ marginBottom: isMediumDevice || isSmallDevice ? "0.2rem" : "" }}>
+          Contact us
+        </a>
       </div>
     </div>
   );
