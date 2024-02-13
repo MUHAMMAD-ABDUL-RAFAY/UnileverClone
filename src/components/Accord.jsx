412ab5eeb5d4b75a338d656029d7a6b688664d1f
MUHAMMAD-ABDUL-RAFAY <k200180@nu.edu.pk>
2024-02-13 23:10:47 +0500

    changes in accordian

--- src/components/Accord.jsx
+++ src/components/Accord.jsx
@@ -7,97 +7,116 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 const Accord = () => {
   return (
     <div style={{ marginLeft: "10%", marginRight: "10%" }}>
-      <Accordion style={{ backgroundColor: "#2A2C2C" }}>
+      {/* backgroundColor #2a2c2c */}
+      <Accordion style={{ backgroundColor: "#efefef" }}>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon style={{ color: "#CFC8BD" }} />}
+        // color #CFC8BD
+          expandIcon={<ExpandMoreIcon style={{ color: "#000" }} />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography style={{ color: "#CFC8BD" }}>COMPANY</Typography>
+          {/* color #CFC8BD */}
+          <Typography style={{ color: "#000" }}>COMPANY</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+        <div className="flex flex-col gap-y-2">
+            <a>Global addresses</a>
+            <a>Strategy</a>
+            <a>Our leadership</a>
+            <a>Brands A - Z</a>
+            <a>Our history</a>
+            <a>Sustainability</a>
+          </div>
         </AccordionDetails>
       </Accordion>
-      <Accordion style={{ backgroundColor: "#2A2C2C" }}>
+      {/* backgroundColor #2a2c2c */}
+      <Accordion style={{ backgroundColor: "#efefef" }}>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon style={{ color: "#CFC8BD" }} />}
+        // color #CFC8BD
+          expandIcon={<ExpandMoreIcon style={{ color: "#000" }} />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography style={{ color: "#CFC8BD" }}>DOWNLOADS</Typography>
+          {/* color #CFC8BD */}
+          <Typography style={{ color: "#000" }}>DOWNLOADS</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          <div className="flex flex-col gap-y-2">
+            <a>Annual Review (pdf, 17Mb)</a>
+            <a>CSV and Sustainability Report (pdf, 15Mb)</a>
+            <a>Responsible Sourcing Standard (pdf, 2Mb)</a>
+          </div>
         </AccordionDetails>
       </Accordion>
-      <Accordion style={{ backgroundColor: "#2A2C2C" }}>
+      {/* backgroundColor #2a2c2c */}
+      <Accordion style={{ backgroundColor: "#efefef" }}>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon style={{ color: "#CFC8BD" }} />}
+        // color #CFC8BD
+          expandIcon={<ExpandMoreIcon style={{ color: "#000" }} />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography style={{ color: "#CFC8BD" }}>OTHER COLGATE BUSINESSES</Typography>
+          {/* color #CFC8BD */}
+          <Typography style={{ color: "#000" }}>OTHER COLGATE BUSINESSES</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+        <div className="flex flex-col gap-y-2">
+            <a>Colgate Health Science</a>
+            <a>Colgate Nespresso</a>
+            <a>Colgate Purina Petcare</a>
+          </div>
         </AccordionDetails>
       </Accordion>
-      <Accordion style={{ backgroundColor: "#2A2C2C" }}>
+      {/* backgroundColor #2a2c2c */}
+      <Accordion style={{ backgroundColor: "#efefef" }}>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon style={{ color: "#CFC8BD" }} />}
+        // color #CFC8BD
+          expandIcon={<ExpandMoreIcon style={{ color: "#000" }} />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography style={{ color: "#CFC8BD" }}>MEDIA</Typography>
+          {/* color #CFC8BD */}
+          <Typography style={{ color: "#000" }}>MEDIA</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+        <div className="flex flex-col gap-y-2">
+            <a>News</a>
+            <a>Media contacts</a>
+            <a>Images</a>
+          </div>
         </AccordionDetails>
+        {/* backgroundColor #2a2c2c */}
       </Accordion>
-      <Accordion style={{ backgroundColor: "#2A2C2C" }}>
+      <Accordion style={{ backgroundColor: "#efefef" }}>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon style={{ color: "#CFC8BD" }} />}
+        // color #CFC8BD
+          expandIcon={<ExpandMoreIcon style={{ color: "#000" }} />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography style={{ color: "#CFC8BD" }}>INVESTORS</Typography>
+          {/* color #CFC8BD */}
+          <Typography style={{ color: "#000" }}>INVESTORS</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+        <div className="flex flex-col gap-y-2">
+            <a>Corporate governance</a>
+            <a>Shares, ADRs, & Bonds</a>
+            <a>Publications</a>
+          </div>
         </AccordionDetails>
       </Accordion>
-      <Accordion className="mb-4" style={{ backgroundColor: "#2A2C2C" }}>
+      <Accordion className="mb-4" style={{ backgroundColor: "#efefef" }}>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon style={{ color: "#CFC8BD" }} />}
+          expandIcon={<ExpandMoreIcon style={{ color: "#000" }} />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography style={{ color: "#CFC8BD" }}>SHARE INFORMATION</Typography>
+          <Typography style={{ color: "#000" }}>SHARE INFORMATION</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          <a>Share Price</a>
         </AccordionDetails>
       </Accordion>
-      <Typography style={{fontSize:"0.875rem",color:"#CFC8BD"}}>Share Price</Typography>
     </div>
   );
 };
